611b2510acf32b0a24c0cdea571018fe12546fdf
zackiehuy <67645086+zackiehuy@users.noreply.github.com>
2020-08-03 22:57:54 +0700

    14

--- models/editor.models.js
+++ models/editor.models.js
@@ -3,14 +3,23 @@ const db = require('../utils/db');
 module.exports = {
     all : function(id){
         return db.load(`SELECT idnews,tittle,abstract as ab,content,name FROM newspaper as n join maincategory as mc
-        on n.idmaincategory = mc.idmaincategory WHERE n.idmaincategory = ${id} and n.status = 4`);
+        on n.idmaincategory = mc.idmaincategory WHERE n.idmaincategory = ${id} and n.status = 4 and n.statusnews = 0`);
     },
     single : function(id){
         return db.load(`SELECT idnews,tittle,abstract as ab,content,n.idmaincategory,name FROM newspaper as n 
         join maincategory as mc on n.idmaincategory = mc.idmaincategory WHERE n.idnews = ${id} and 
-        n.status = 4`);
+        n.status = 4 and n.statusnews = 0`);
     },
-    subcategory : function(id){
-        return db.load(`SELECT idsubcategory,name FROM subcategory`);
+    subcategory : function(){
+        return db.load(`SELECT idsubcategory,name as names FROM subcategory WHERE status = 0`);
+    },
+    maincategory : function(id){
+        return db.load(`SELECT idmaincategory,name FROM maincategory WHERE status = 0 and idmaincategory != ${id}`);
+    },
+    tag : function(id){
+        return db.load(`SELECT idtag,name FROM tag WHERE status = 0 and idnews = ${id}`);
+    },
+    singlecategory : function(id){
+        return db.load(`SELECT idmaincategory,name FROM maincategory WHERE status = 0 and idmaincategory = ${id}`);
     }
 }
--- routes/editor.routes.js
+++ routes/editor.routes.js
@@ -3,11 +3,34 @@ const model_editor = require('../models/editor.models');
 const router = express.Router();
 
 router.get('/list', async function(req,res ){
-    const list = model_editor.all(1);
+    const list = await model_editor.all(1);
     res.render('admin/Editor/list',{
         list,
         emptylist : list.length === 0
     });
-})
+});
+
+router.get('/edit', async function(req,res){
+    const maincategory = await model_editor.maincategory(1);
+    const row = await model_editor.singlecategory(1);
+    const subcategory = await model_editor.subcategory();
+    const tag = await model_editor.tag(1);
+    res.render('admin/Editor/edit',{
+        maincategory,
+        subcategory,
+        row,
+        tag,
+        tagempty : tag.length === 0
+    })
+});
+
+router.get('/denide', function(req,res ){
+    res.render('admin/Editor/denide');
+});
 
 module.exports = router;
+
+
+
+
+
